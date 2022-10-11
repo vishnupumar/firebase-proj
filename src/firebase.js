925dcb3d5a8ebf404f50ad1861e4fa14eca9b21e
vishnu pumar <vishnupumar1996@gmail.com>
2022-10-11 09:49:39 +0530

    second2

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRDlVf3x_3r5jtEFVZ2Lutnzj84Nv_bTc",
-  authDomain: "otp-app-demo-ce64b.firebaseapp.com",
-  projectId: "otp-app-demo-ce64b",
-  storageBucket: "otp-app-demo-ce64b.appspot.com",
-  messagingSenderId: "833918803407",
-  appId: "1:833918803407:web:74db0b88fa4e57153186ba",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId:appId,
 };
 
 // Initialize Firebase
